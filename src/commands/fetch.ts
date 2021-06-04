ddb3321ded2367e2fde89fb618df7e7785ed3e42
Sandro Peirano <sandro.peirano@gdpteam.com>
2021-06-04 18:48:44 -0500

    Add scrapping functionality

--- src/commands/fetch.ts
+++ src/commands/fetch.ts
@@ -1,16 +1,24 @@
 import SciHubService from "../services/scihub";
-
+import ScrappingService from "../services/scrapper";
 import Command from "../types/commands.interface";
 
 const fetch: Command = {
   name: "fetch",
   description: "Fetches a paper from scihub",
   execute: async (message, args): Promise<void> => {
-    message.channel.send("Fetching document with DOI " + args);
-    console.log(message.author, args);
+    try {
+      message.channel.send(`Fetching document with DOI ${args[0]}`);
+      console.log(message.author, args);
 
-    const data = await SciHubService.getPaper(args);
-    console.log(data);
+      const data = await SciHubService.getPaper(args);
+      console.log(data);
+      const downloadLink = ScrappingService.getDownloadLink(data);
+
+      message.channel.send(`Link found: ${downloadLink}`);
+    } catch (error) {
+      console.log(error);
+      message.channel.send("An unexpect error occurred while fetching");
+    }
   },
 };
 
